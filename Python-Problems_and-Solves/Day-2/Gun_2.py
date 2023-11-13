f5c41fb3cca5b3ff048188bf238a44ba1ddd72d1
Cesurbedel <150424156+Cesurbedel@users.noreply.github.com>
2023-11-13 19:09:19 +0300

    Add files via upload

---
+++ Python-Problems_and-Solves/Day-2/Gun_2.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    a = int(input(""))
+    b = int(input(""))
+
+    # Sum
+    sum_result = a + b
+    print(sum_result)
+
+    # Difference
+    diff_result = a - b
+    print(diff_result)
+
+    # Product
+    product_result = a * b
+    print(product_result)
