9775ffa6bb8a08883d5a7ddfa3b651892ce60b0f
Cesurbedel <150424156+Cesurbedel@users.noreply.github.com>
2023-11-14 13:55:36 +0300

    Gun_3

---
+++ Python-Problems_and-Solves/Day-3/Gun_3.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    # Read two integers from STDIN
+    a = int(input(""))
+    b = int(input(""))
+
+    # Perform integer division (//) and print the result
+    print(a // b)
+
+    # Perform float division (/) and print the result
+    print(a / b)
