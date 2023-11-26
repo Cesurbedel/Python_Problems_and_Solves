f608dafcb3c2cfcc82deb1e7dfa765975191cc15
Cesurbedel <150424156+Cesurbedel@users.noreply.github.com>
2023-11-26 13:16:06 +0300

    Add files via upload

---
+++ Python-Problems_and-Solves/Day-13/Gun_13b.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    start_year = int(input("Enter the starting year: "))
+    end_year = int(input("Enter the ending year: "))
+
+    # Creating a range of years and converting it to a list
+    years_list = list(range(start_year, end_year))
+
+    # Printing the list of years
+    print(years_list)
+
